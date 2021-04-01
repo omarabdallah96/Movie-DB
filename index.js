87bf851de94aef030768e98f73c8daf744dc52d4
omar abdallah <omarlb512@gmail.com>
2021-04-01 20:32:25 +0300

    step 3

--- index.js
+++ index.js
@@ -6,4 +6,25 @@ app.get('/', (req, res) => {
     res.send('ok')
 });
 
-app.listen(port, () => console.log(`Success! Your application is running on \"http://localhost:${port}\"`)); 
+
+
+
+
+const d = new Date();
+const TIME = d.getHours() + ":" + d.getSeconds();
+
+
+app.get('/', (req, res) => {
+    res.send('ok')
+});
+
+app.get('/test', (req, res) => {
+    res.status(200).send('ok');
+});
+
+app.get('/time', (req, res) => {
+    let t = TIME;
+    res.status(200).send(String(t));
+});
+
+app.listen(port, () => console.log(`Success! Your application is running on \"http://localhost:${port}\"`));
