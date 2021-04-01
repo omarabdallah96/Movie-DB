33f2d2ce2e8eb8e0ce094748dd7063ba6190a4ac
omar abdallah <omarlb512@gmail.com>
2021-04-01 21:48:30 +0300

    step 4

--- index.js
+++ index.js
@@ -26,5 +26,32 @@ app.get('/time', (req, res) => {
     let t = TIME;
     res.status(200).send(String(t));
 });
+app.get('/', (req, res) => {
+    res.send('ok');
+  });
 
+  app.get('/test', (req, res) => {
+    res.status(200).send('message:"ok"');
+  });
+  var date = new Date();
+  app.get('/time', (req, res) => {
+    res.status(200).send(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+  });
+
+  app.get('/hello', (req, res) => {
+    res.status(200).send('Hello')
+  });
+
+  app.get('/hello/:id', (req, res) => {
+    res.status(200).send('Hello ' + req.params.id)
+  });
+
+
+  app.get('/search', (req, res) => {
+    if (req.query.s == "" || req.query.s == undefined) {
+      res.status(500).send('you have to provide a search');
+    } else {
+      res.status(200).send('ok ' + req.query.s)
+    }
+  });
 app.listen(port, () => console.log(`Success! Your application is running on \"http://localhost:${port}\"`));
